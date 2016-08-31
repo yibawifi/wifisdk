7e3f6fe7258e1625ec0efb6037e046e0d92c4f20
zhaoyanjun <362299465@qq.com>
2016-08-31 19:32:20 +0800

    add sdk demo

---
+++ app/src/main/java/com/yiba/MainActivity.java
@@ -0,0 +1,25 @@
+package com.yiba;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import www.yiba.com.wifisdk.activity.YIbaWifiActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById( R.id.openWifi).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //打开wifi 界面
+                Intent intent = new Intent( MainActivity.this , YIbaWifiActivity.class) ;
+                startActivity( intent );
+            }
+        });
+
+    }
+}
