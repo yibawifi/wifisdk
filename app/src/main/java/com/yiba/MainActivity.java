7993f30cc8f7c944c987c39a4129eccbd62e2831
zhaoyanjun <362299465@qq.com>
2017-01-20 17:28:42 +0800

    添加MyApplication类

--- app/src/main/java/com/yiba/MainActivity.java
+++ app/src/main/java/com/yiba/MainActivity.java
@@ -2,11 +2,8 @@ package com.yiba;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.LayoutInflater;
 import android.view.View;
 import www.yiba.com.wifisdk.activity.YIbaWifiActivity;
-import www.yiba.com.wifisdk.ad.CustomerViewBuilder;
-import www.yiba.com.wifisdk.manager.WiFiSDKManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        addBannerAd();
         findViewById( R.id.openWifi).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,21 +22,4 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-
-    /**
-     * 允许你添加一个 banner 广告到 wifi 列表。
-     * 使用此功能请联系：support@pegasus-mobile.com
-     */
-    private void addBannerAd(){
-        WiFiSDKManager.getInstance().setBannerAdInWifiList(MainActivity.this, new CustomerViewBuilder.CustomerAdView() {
-            @Override
-            public View createView(CustomerViewBuilder.NativeAdControl nativeAdControl) {
-                LayoutInflater layoutInflater = LayoutInflater.from( MainActivity.this ) ;
-                View view = layoutInflater.inflate( R.layout.banner_ad_layout , null ) ;
-                nativeAdControl.show( getApplicationContext() );
-                return view ;
-            }
-        })  ;
-    }
-
 }
